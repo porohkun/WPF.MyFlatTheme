1cf408179bd1fa71fe2cd539906108e0aba51769
poroh-kun <meta.poroh.kun@gmail.com>
2020-10-12 17:21:02 +0300

    chrome window style

---
+++ Commands/CloseWindowCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPF.MyFlatTheme.Commands
+{
+    public class CloseWindowCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (!(parameter is Window window))
+                return false;
+
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            var window = parameter as Window;
+
+            window.Close();
+        }
+    }
+}
---
+++ Commands/MaximizeResoreWindowCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPF.MyFlatTheme.Commands
+{
+    public class MaximizeResoreWindowCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (!(parameter is Window window))
+                return false;
+
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            var window = parameter as Window;
+
+            if (window.WindowState == WindowState.Maximized)
+                window.WindowState = WindowState.Normal;
+            else
+                window.WindowState = WindowState.Maximized;
+        }
+    }
+}
